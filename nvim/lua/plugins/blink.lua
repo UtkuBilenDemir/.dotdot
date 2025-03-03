d525b82a21ed45fa0dc305301cb4fb0a709428e7
Utku Bilen Demir <84389167+UtkuBilenDemir@users.noreply.github.com>
2025-03-03 22:12:03 +0100

    20250303-2

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -16,45 +16,27 @@ return {
   },
   opts = {
     completion = {
-      trigger = {
-        show_on_trigger_character = true,
-        show_on_accept_on_trigger_character = true,
-      },
-      list = {
-        max_items = 3,
-      },
-      documentation = {
-        auto_show_delay_ms = 500000000000,
+      ghost_text = {
+        enabled = false,
+        --show_with_selection = false,
       },
     },
     sources = {
-      default = { "snippets", "path", "luasnip", "lsp", "buffer" },
+      default = { "snippets", "path", "lsp", "buffer" },
       providers = {
         lsp = {
           name = "lsp",
           enabled = true,
           module = "blink.cmp.sources.lsp",
           kind = "LSP",
-          -- When linking markdown notes, I would get snippets and text in the
-          -- suggestions, I want those to show only if there are no LSP
-          -- suggestions
-          fallbacks = { "snippets", "luasnip", "buffer" },
-          score_offset = 950, -- the higher the number, the higher the priority
-        },
-        luasnip = {
-          name = "luasnip",
-          enabled = true,
-          module = "blink.cmp.sources.luasnip",
-          score_offset = 1000, -- the higher the number, the higher the priority
+          fallbacks = { "snippets", "buffer" },
+          score_offset = 950,
         },
         path = {
           name = "Path",
           module = "blink.cmp.sources.path",
           score_offset = 3,
-          -- When typing a path, I would get snippets and text in the
-          -- suggestions, I want those to show only if there are no path
-          -- suggestions
-          fallbacks = { "snippets", "luasnip", "buffer" },
+          fallbacks = { "snippets", "buffer" },
           opts = {
             trailing_slash = false,
             label_trailing_slash = true,
@@ -68,57 +50,44 @@ return {
           name = "snippets",
           enabled = true,
           module = "blink.cmp.sources.snippets",
-          score_offset = 900, -- the higher the number, the higher the priority
+          -- preset = "luasnip",
+          score_offset = 1000,
           opts = {
             search_paths = { "/Users/ubd/.config/nvim/lua/snips" },
           },
         },
-        -- Example on how to configure dadbod found in the main repo
-        -- https://github.com/kristijanhusak/vim-dadbod-completion
         dadbod = {
           name = "Dadbod",
           module = "vim_dadbod_completion.blink",
-          score_offset = 950, -- the higher the number, the higher the priority
+          score_offset = 950,
         },
       },
     },
-    -- This comes from the luasnip extra, if you don't add it, won't be able to
-    -- jump forward or backward in luasnip snippets
-    -- https://www.lazyvim.org/extras/coding/luasnip#blinkcmp-optional
-    snippets = {
-      expand = function(snippet)
-        require("luasnip").lsp_expand(snippet)
-      end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require("luasnip").jumpable(filter.direction)
-        end
-        return require("luasnip").in_snippet()
-      end,
-      jump = function(direction)
-        require("luasnip").jump(direction)
-      end,
-    },
-    -- The default preset used by lazyvim accepts completions with enter
-    -- I don't like using enter because if on markdown and typing
-    -- something, but you want to go to the line below, if you press enter,
-    -- the completion will be accepted
-    -- https://cmp.saghen.dev/configuration/keymap.html#default
+    -- snippets = {
+    --   expand = function(snippet)
+    --     require("luasnip").lsp_expand(snippet)
+    --   end,
+    --   active = function(filter)
+    --     if filter and filter.direction then
+    --       return require("luasnip").jumpable(filter.direction)
+    --     end
+    --     return require("luasnip").in_snippet()
+    --   end,
+    --   jump = function(direction)
+    --     require("luasnip").jump(direction)
+    --   end,
+    -- },
     keymap = {
       preset = "default",
       ["<C-z>"] = { "select_and_accept", "fallback" },
-
       ["<Tab>"] = { "snippet_forward", "fallback" },
       ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
       ["<Up>"] = { "select_prev", "fallback" },
       ["<Down>"] = { "select_next", "fallback" },
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
-
       ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
       ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
       ["<C-e>"] = { "hide", "fallback" },
     },
