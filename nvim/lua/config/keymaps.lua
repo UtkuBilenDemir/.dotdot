8ff69764a1c4df9a27a1b653d6199ed0396da26b
Utku Bilen Demir <84389167+UtkuBilenDemir@users.noreply.github.com>
2026-03-28 00:56:11 +0100

    Skip checktime if file was renamed by template (avoids E211)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -71,13 +71,19 @@ end, { desc = "Obsidian: new note" })
 map("n", "<leader>oZ", function()
   -- Apply zettel.js: renames file to title, sets 02_zet tag, formats structure
   vim.fn.system("open -g 'obsidian://adv-uri?vault=rhizome&commandid=templater-obsidian:08_templates/00_Zettelkasten_Template.md'")
-  vim.defer_fn(function() vim.cmd("checktime") end, 2000)
+  local path = vim.fn.expand("%:p")
+  vim.defer_fn(function()
+    if vim.fn.filereadable(path) == 1 then vim.cmd("checktime") end
+  end, 2000)
 end, { desc = "Obsidian: format zettel" })
 
 map("n", "<leader>oM", function()
   -- Apply MOC template via moc.js
   vim.fn.system("open -g 'obsidian://adv-uri?vault=rhizome&commandid=templater-obsidian:08_templates/01_MOC_Template.md'")
-  vim.defer_fn(function() vim.cmd("checktime") end, 2000)
+  local path = vim.fn.expand("%:p")
+  vim.defer_fn(function()
+    if vim.fn.filereadable(path) == 1 then vim.cmd("checktime") end
+  end, 2000)
 end, { desc = "Obsidian: new MOC" })
 
 -- Delete note
