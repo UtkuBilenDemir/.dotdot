21a0443eb410309a8ed852d8fd87f7fae56a6a4e
Utku Bilen Demir <84389167+UtkuBilenDemir@users.noreply.github.com>
2026-03-28 01:01:53 +0100

    Fix checktime augroup name: use lazyvim_checktime to actually override LazyVim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,6 +5,6 @@
 -- Override LazyVim's checktime autocmd to suppress E211 when files are
 -- renamed externally (e.g. Obsidian Templater renames notes after applying).
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-  group = vim.api.nvim_create_augroup("checktime", { clear = true }),
+  group = vim.api.nvim_create_augroup("lazyvim_checktime", { clear = true }),
   command = "silent! checktime",
 })
